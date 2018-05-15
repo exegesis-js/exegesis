89bda37dc09de2ff2a04a268bb0f48b1ba9e553c
Jason Walton <dev@lucid.thedreaming.org>
2018-05-14 23:45:58 -0400

    fix: Fix httpHasBody()

--- src/utils/httpHasBody.ts
+++ src/utils/httpHasBody.ts
@@ -1,5 +1,5 @@
 export default function httpHasBody(headers: {[header: string]: any}) : boolean {
     const contentLength = headers['content-length'];
     return !!headers['transfer-encoding'] ||
-            (contentLength && typeof(contentLength) === 'number' && contentLength > 0);
+        (contentLength && contentLength !== '0' && contentLength !== 0);
 }
