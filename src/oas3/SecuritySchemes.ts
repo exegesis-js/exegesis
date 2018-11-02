4a4836aaa920add2089d1c683fe2d1a8f5d56479
Jason Walton <github@lucid.thedreaming.org>
2018-11-01 23:40:22 -0400

    fix: Upgrade openapi3-ts.  Support ref for openApiDoc.components.securitySchemes.

--- src/oas3/SecuritySchemes.ts
+++ src/oas3/SecuritySchemes.ts
@@ -1,34 +1,39 @@
 import ld from 'lodash';
 import * as oas3 from 'openapi3-ts';
 import { AuthenticatorInfo } from '..';
+import { resolveRef } from '../utils/json-schema-resolve-ref';
 
 export default class SecuritySchemes {
-    private readonly _securitySchemes: {[securityScheme: string]: oas3.SecuritySchemeObject};
-    private readonly _challenges: {[securityScheme: string]: string | undefined};
-    private readonly _infos: {[securityScheme: string]: AuthenticatorInfo};
+    private readonly _securitySchemes: { [securityScheme: string]: oas3.SecuritySchemeObject };
+    private readonly _challenges: { [securityScheme: string]: string | undefined };
+    private readonly _infos: { [securityScheme: string]: AuthenticatorInfo };
 
     constructor(openApiDoc: oas3.OpenAPIObject) {
-        this._securitySchemes = openApiDoc.components && openApiDoc.components.securitySchemes || {};
+        const securitySchemes =
+            (openApiDoc.components && openApiDoc.components.securitySchemes) || {};
+        this._securitySchemes = securitySchemes.ref
+            ? resolveRef(openApiDoc, securitySchemes)
+            : securitySchemes;
 
         this._challenges = ld.mapValues(this._securitySchemes, scheme => {
-            if(scheme.type === 'http') {
+            if (scheme.type === 'http') {
                 return scheme.scheme || 'Basic';
             }
-            if(scheme.type === 'oauth2' || scheme.type === 'openIdConnect') {
+            if (scheme.type === 'oauth2' || scheme.type === 'openIdConnect') {
                 return 'Bearer';
             }
             return undefined;
         });
 
         this._infos = ld.mapValues(this._securitySchemes, scheme => {
-            if(scheme.type === 'apiKey') {
+            if (scheme.type === 'apiKey') {
                 return {
-                    in: (scheme.in as any),
-                    name: scheme.name
+                    in: scheme.in as any,
+                    name: scheme.name,
                 };
-            } else if(scheme.type === 'http') {
+            } else if (scheme.type === 'http') {
                 return {
-                    scheme: scheme.scheme
+                    scheme: scheme.scheme,
                 };
             } else {
                 return {};
@@ -36,12 +41,11 @@ export default class SecuritySchemes {
         });
     }
 
-    getChallenge(schemeName: string) : string | undefined {
+    getChallenge(schemeName: string): string | undefined {
         return this._challenges[schemeName];
     }
 
-    getInfo(schemeName: string) : AuthenticatorInfo {
+    getInfo(schemeName: string): AuthenticatorInfo {
         return this._infos[schemeName];
     }
-
 }
