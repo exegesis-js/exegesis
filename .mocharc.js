66c196f73df845739f80e2158cfecb9518bffcd3
Jason Walton <github@lucid.thedreaming.org>
2020-07-14 14:32:35 -0400

    chore: Upgrade mocha to 8.0.0.

---
+++ .mocharc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    extension: ['js', 'jsx', 'ts', 'tsx'],
+    require: ['ts-node/register'],
+    recursive: true,
+    fullTrace: true,
+    checkLeaks: true,
+    reporter: 'spec',
+};
